30cfc1f35309f583703fe0f305b020774d6de364
Tom <tomervingmz@gmail.com>
2025-12-13 07:13:47 +0800

    Iniital commit

---
+++ src/app/landingPage/page.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+export default function LandingPage(){
+  return (
+    <Box>
+      <Typography variant="h1">
+        Welcome to DocuTrack
+      </Typography>
+    </Box>
+  );
+}

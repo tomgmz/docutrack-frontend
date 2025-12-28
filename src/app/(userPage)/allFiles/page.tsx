12ce400c665ca0b38140ef6b097af7e96194215f
Tom <tomervingmz@gmail.com>
2025-12-29 03:04:35 +0800

    layout files sorted out

--- src/app/(userPage)/allFiles/page.tsx
+++ src/app/(userPage)/allFiles/page.tsx
@@ -2,7 +2,7 @@
 
 export default function StaredPage(){
   return (
-    <div className="flex items-center justify-center flex-1">
+    <div className="flex items-center justify-center h-full w-full">
       <p>ALL FILES PAGE</p>
     </div>
   );

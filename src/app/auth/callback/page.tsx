b68b2df8d104ba00e536ae025cbc2e594bff6068
Tom <tomervingmz@gmail.com>
2026-01-07 20:01:33 +0800

    callback

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -7,16 +7,7 @@ export default function AuthCallback() {
   const router = useRouter();
 
   useEffect(() => {
-    const hash = window.location.hash.substring(1);
-    const params = new URLSearchParams(hash);
-    const accessToken = params.get("access_token");
-
-    if (accessToken) {
-      localStorage.setItem("supabase_access_token", accessToken);
-      router.replace("/userHome");
-    } else {
-      console.error("No access token found in callback");
-    }
+    router.replace("/userHome");
   }, [router]);
 
   return <div>Signing you in...</div>;
